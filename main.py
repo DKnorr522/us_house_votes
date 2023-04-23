a6c9719cdf392e582cd08bf68e83ecbfe23d0d78
Dan Knorr <Daniel.Knorr@gmail.com>
2023-04-23 00:40:32 -0400

    try to display all roll call votes

--- main.py
+++ main.py
@@ -7,7 +7,16 @@ def main():
     db_path = "congress_roll_calls.db"
     conn = sqlite3.connect(db_path)
     cur = conn.cursor()
+
     st.header("Main Title")
+
+    all_rolls = pd.read_sql_query(
+        sql="""
+            select * from rolls
+        """,
+        con=conn
+    )
+    st.dataframe(all_rolls)
 
 
 if __name__ == "__main__":
