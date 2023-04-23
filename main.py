c02a9967972ace361c1b1aa67e06477fe34846f0
Dan Knorr <Daniel.Knorr@gmail.com>
2023-04-23 02:15:12 -0400

    added functionality

--- main.py
+++ main.py
@@ -169,6 +169,7 @@ def roll_vote_count(roll_id, conn):
 def fetch_all_rolls_with_votes(conn, latest_roll_call):
     rolls_list = []
     for roll_call in range(1, latest_roll_call + 1):
+        print(f"{roll_call = }")
         rolls_list.append(roll_vote_count(roll_call, conn))
     return pd.concat(rolls_list, axis=1)
 
